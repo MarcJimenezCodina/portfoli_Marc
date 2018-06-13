40aacbc0ffd059f19527d7ca7aaaaee3d87c963c
MarcJimenezCodina <39918433+MarcJimenezCodina@users.noreply.github.com>
2018-06-13 19:52:19 +0200

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,23 @@
+document.getElementById("on").onclick = function(){
+    if(document.getElementById("pantallareves").className == "movimentreves"){
+        document.getElementById("pantallareves").className = "movimentfinalreves";
+    }else{
+        document.getElementById("pantallareves").className= "movimentreves";
+    }
+    if(document.getElementById("pantalla").className == "moviment"){
+        
+        document.getElementById("pantalla").className = "movimentfinal";
+    }else{
+        
+        document.getElementById("pantalla").className= "moviment";
+    }
+    }
+
+
+
+
+
+
+
+
+    
